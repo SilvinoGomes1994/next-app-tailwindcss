8fcf5d912009ac1384b2cbdad41c6191ffc495eb
Silvino Gomes <silvinog97@gmail.com>
2025-05-30 17:06:40 +0100

    Adicionado os botões login & register, configurados no Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -33,21 +33,27 @@ const Header = () => {
     <div className='container mx-auto h-full flex items-center justify-between'>
       {/* logo */}
        <Link href=''>
-       <Image src={'/assets/img/logo4.png'}  width={117} height={55} alt='logo-Fitness' />
+       <Image src={'/assets/img/logo4.png'}  width={117} height={55} alt='logo-Fitness' className='text-white' />
        </Link>
       {/* Mobile Nav - hidden on large devices */}
       <MobileNav containerStyles={`${headerActive ? 'top-[90px]': 'top-[124px]'} 
       ${openNav ? 'max-h-max pt-8 pb-10 border-t border-white/10' : 'max-h-0 pt-0 pb-0 overflow-hidden border-white/0'}
       flex flex-col text-center gap-8 fixed bg-zinc-950 w-full left-0 text-base uppercase font-medium text-white transition-all xl:hidden `} />
       {/* Desktop Nav - hidden on Small devices*/}
-    <Nav containerStyles='flex gap-4 text-white hidden xl:flex'/>
+    <Nav containerStyles='flex gap-4 text-white text-base uppercase font-medium transition-all hidden xl:flex'/>
     {/* hide/open menu button */}
-    <div>
-      
+    <div className='flex items-center gap-4'>
+      {/* login & register buttons*/}
+      <div className='text-white flex items-center gap-4'>
+        <button className='hover:text-red-700 transition-all text-base uppercase font-medium'>login</button>
+        <button className='hover:text-red-700 transition-all text-base uppercase font-medium'>register</button>
+      </div>
+
       <button onClick={()=> setOpenNav(!openNav)} className='text-white xl:hidden'>
         <MdMenu className='text-4xl'/>
       </button>
     </div>
+
     </div>
 
   </header>
