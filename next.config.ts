73703082201a0966be0bcfe24cb311a6c1f2851c
Silvino Gomes <silvinog97@gmail.com>
2025-07-06 22:17:04 +0100

    Configurado o next.config.ts e .github\workflows-> nextjs.yml

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-
+   // Exportação estática
+  reactStrictMode: true,
+  trailingSlash: true, // adiciona / ao final das URLs (recomendado para exportação)
   // Ocultar o logotipo do Next na minha página
   devIndicators: false,
+  images: { unoptimized: true }
 };
 
 export default nextConfig;
