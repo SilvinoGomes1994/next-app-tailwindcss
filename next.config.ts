9fbccfeffedf142cf130c5cbfb279c9322523179
Silvino Gomes <silvinog97@gmail.com>
2025-07-07 00:37:56 +0100

    Código Finalizado

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',           // exportação estática
-  distDir: 'out',             // pasta de build (opcional)
+  distDir: 'dist',             // pasta de build (opcional)
   reactStrictMode: true,      // boa prática
   trailingSlash: true,        // importante para exportação funcionar corretamente
   experimental: {
@@ -11,8 +11,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,        // necessário para exportação estática
   },
-  basePath: "/next-app-tailwindcss",
-  assetPrefix: "/next-app-tailwindcss",
 };
 
 export default nextConfig;
